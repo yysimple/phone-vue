9e4795ed6b7a683b7f94b97c44f6c0b5aadeffa6
yysimple <1449697757@qq.com>
2019-12-18 17:37:02 +0800

    修改AddPhoneInfo.vue的无法添加内容

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ import HomeUser from './views/Home-User.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
